65790f32ca08d93fbce3382dfb38dddabde129ab
penholder <penhold3r@gmail.com>
2019-11-28 14:06:48 -0300

    new sections

---
+++ src/components/Specials.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Specials = () => {
+	return (
+		<section className="specials">
+			<h2>Nuestros Especiales</h2>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex ducimus mollitia commodi
+				sint provident dolor eligendi sequi ipsam, optio voluptatibus.
+			</div>
+		</section>
+	)
+}
+
+export default Specials
